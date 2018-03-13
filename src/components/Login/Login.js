aa5e600170f2aa06b550ad9b00c1474d3c1177a4
AlejandroVC67 <vcalejandro97@gmail.com>
2018-03-12 20:38:48 -0500

    first commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react'
+import './_login.scss'
+
+export default class Login extends Component {
+  render () {
+    return (
+      <Fragment>
+        <div className='login'>
+          <div className='login__image-container' />
+        </div>
+      </Fragment>
+    )
+  }
+}
