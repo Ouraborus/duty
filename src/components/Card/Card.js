3f8eaab81317375af6915a292ccc7ad2fc140ce8
Ouraborus <1222eca@gmail.com>
2018-03-15 19:24:53 -0500

    Fixed Card and Navbar bugs

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -9,22 +9,27 @@ export default class Card extends Component {
   }
   render () {
     return (
-      <div className='card-wide mdl-card mdl-shadow--2dp'>
-        <div className='mdl-card__title'>
-          <h2 className='mdl-card__title-text'>Q'bano</h2>
-        </div>
-        <div className='mdl-card__supporting-text'>
+      <div className='grid'>
+        <div className=' card-wide mdl-card mdl-shadow--2dp'>
+          <div className='card mdl-card__title'>
+            <h2 className='card__text card__text--left'>Q'bano</h2>
+            <p className='card__text card__text--right'>Mesero</p>
+            <p className='card__text card__text--right'>01/01/2018 - 03/01/2018</p>
+            <p className='card__text card__text--right'>COP $ 300.000</p>
+          </div>
+          <div className='mdl-card__supporting-text'>
         Se requiere mesero...
-        </div>
-        <div className='mdl-card__actions mdl-card--border'>
-          <a className='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect'>
-            Get Started
-          </a>
-        </div>
-        <div className='mdl-card__menu'>
-          <button className='mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect'>
-            <i className='material-icons'>share</i>
-          </button>
+          </div>
+          <div className='mdl-card__actions mdl-card--border'>
+            <a className='mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect'>
+            Ver Más
+            </a>
+          </div>
+          <div className='mdl-card__menu'>
+            <button className='mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect'>
+              <i className='material-icons'>delete</i>
+            </button>
+          </div>
         </div>
       </div>
     )
