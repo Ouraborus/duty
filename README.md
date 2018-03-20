d124f1abb4b264216e1072178b60c941defa8ff6
Ouraborus <1222eca@gmail.com>
2018-03-19 23:07:12 -0500

    Changing md file

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # DUTY
 ## How to run
-    -Install npm
-    -Install dependencies, by running `npm i`
-    -Run project, by executing at CLI  `npm run start`
-    -At your browser - `localhost:8080`
-## Developed by:
-    Esteban Cardona Álvarez
-    Alejandro Villa Cárdenas
-    Pablo Wolff Velásquez
+Install **npm**
+
+Install dependencies, by running `npm i`
+
+Run project, by executing at CLI  `npm run start`
+
+At your browser:  `localhost:8080`
+
+## Developed by
+Esteban Cardona Álvarez
+
+Alejandro Villa Cárdenas
+
+Pablo Wolff Velásquez
--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Fragment} from 'react'
 import NavBar from './components/NavBar/NavBar'
 // import Login from './components/Login/Login'
-import SignUp from './components/SignUp/SignUp'
+// import SignUp from './components/SignUp/SignUp'
+import Card from './components/Card/Card'
 import './scss/_reset.scss'
 import './scss/_fonts.scss'
 import './scss/_app.scss'
@@ -9,11 +10,9 @@ import './scss/_app.scss'
 export default function App () {
   return (
     <Fragment>
-<<<<<<< HEAD
       <NavBar />
       {/* <Login /> */}
-      <SignUp />
-=======
+      {/* <SignUp /> */}
       <div className='app-container'>
         <NavBar />
         <div className='grid'>
@@ -21,7 +20,6 @@ export default function App () {
           <Card />
         </div>
       </div>
->>>>>>> c128007d1b1ccee6c599e66bb107a1c26f957fe5
     </Fragment>
   )
 }
