1d42bb0c48766271008c31c5e727d8f3fc57b6c9
AlejandroVC67 <vcalejandro97@gmail.com>
2018-04-10 16:27:43 -0500

    initial firebase integration approach

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,24 +1,30 @@
 import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
+import loginUtils from '../../firebase/Firebase'
 import './_login.scss'
 import '../../scss/_icon.scss'
 
 export default class Login extends Component {
+  constructor () {
+    super()
+    this.handleLogin = this.handleLogin.bind(this)
+  }
+
+  handleLogin () {
+    loginUtils(this.user.value, this.pass.value)
+  }
+
   render () {
     return (
       <Fragment>
         <div className='login'>
           <div className='login__image-container' />
           <section className='login__form'>
-            <input className='login__form-input' placeholder='Username or Email' type='text' required />
-            <input className='login__form-input' placeholder='Password' type='password' required />
-            {/* <button className='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent login__form-button'>
-            </button> */}
-            <div className='login__button'>
-              <Link to='/dashboard' className='login__button-text'>Entrar</Link>
+            <input className='login__form-input' placeholder='Username or Email' type='text' required ref={(user) => { this.user = user }} />
+            <input className='login__form-input' placeholder='Password' type='password' required ref={(pass) => { this.pass = pass }} />
+            <div className='login__button' onClick={this.handleLogin}>
+              {/* <Link to='/dashboard' className='login__button-text'>Entrar</Link> */}
             </div>
-            {/* <button className='mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent login__form-button'>
-              </button> */}
             <div>
               <Link to='/registrarse'>¿No estás registrado aún?</Link>
             </div>
